b1fd7dcf79c922c0cc122177915a75a6867477a8
yuki <yuki.asano.0107@gmail.com>
2019-11-24 15:30:04 +0000

    rails05 課題

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,15 @@ Rails.application.routes.draw do
 end
 
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'pages/help'
   get 'pages/index'
   root 'pages#index'
   
   resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
